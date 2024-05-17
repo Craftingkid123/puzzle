039d72d791088918e7cfdb57813f6a2724d90f0e
Carter Brown <59338812+Craftingkid123@users.noreply.github.com>
2024-05-17 18:41:37 -0400

    no allowing empty name slots on multiplayer leader select page

--- nameScript.js
+++ nameScript.js
@@ -1,14 +1,18 @@
 function printName() {
     var name = document.getElementById("leaderFname").value;
+
+    if (name == ""){
+    } else {
+
     document.getElementById("printName").innerHTML = "Hello " + name + ", your mission is simple, complete all five tasks by working together. Good Luck! To begin your challange press the button below:";
 
-    var nextButton = document.createElement("INPUT");
-    nextButton.setAttribute("type", "button");
-    nextButton.setAttribute("value", "Start First Challange!");
-    nextButton.setAttribute("onclick", "challangeOne()");
-    nextButton.setAttribute("class", "nextButton");
-    document.body.appendChild(nextButton);
-  
+        var nextButton = document.createElement("INPUT");
+        nextButton.setAttribute("type", "button");
+        nextButton.setAttribute("value", "Start First Challange!");
+        nextButton.setAttribute("onclick", "challangeOne()");
+        nextButton.setAttribute("class", "nextButton");
+        document.body.appendChild(nextButton);
+    }
 }
 
 function challangeOne() {
